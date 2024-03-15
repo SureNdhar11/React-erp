7c598ecc154af13f05920a4523c39412fe95de60
SureNdhar11 <131585593+SureNdhar11@users.noreply.github.com>
2024-03-15 19:48:47 +0530

    Update dashboard.js

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,22 +1,15 @@
-import React from 'react'
-import { Link } from 'react-router-dom/cjs/react-router-dom'
-import { ButtonContainer } from './Button'
+import React from 'react';
+import '../App.css';
+//import { Link, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+//import { ButtonContainer } from './Button';
+import dashboardImage from '../img/dashboardImage.jpeg'; // Import your image
+//import './dash.css'; // Import your custom CSS file for styling
 
 export default function Dashboard() {
     return (
-        <div className="container mt-5">
-            <div className="row">
-                <div className="col-10 mx-auto text-center text-title text-primary">
-                    <h1>Welcome all For My Shopping Page</h1>
-                </div>
-                {/* <div className="col-10 mx-auto  text-center">
-                    <Link to="/pro">
-                        <ButtonContainer>
-                            back to shopping
-                        </ButtonContainer>
-                    </Link>
-                </div> */}
-            </div>
+        <div className="dashboard-container">
+            <img src={dashboardImage} alt="Dashboard Image" className="dashboard-image" />
+            
         </div>
-    )
+    );
 }
